471aaae74f1eeecd4d96e96afd648cb8179799eb
Younes Henni <younabobo3@gmail.com>
2020-06-07 19:46:47 +0100

    test

--- files/lab1.py
+++ files/lab1.py
@@ -11,6 +11,8 @@ def topology():
                   link=TCLink, switch=OVSKernelSwitch)
     net = Mininet(controller=Controller, switch=OVSSwitch)
     # Add hosts and switches
+    c0 = net.addController(
+        'c0', controller=Controller, ip='127.0.0.1', port=6633)
     h1 = net.addHost('h1', ip='10.1.1.10/24', mac='00:00:00:00:00:01')
     h2 = net.addHost('h2', ip='10.1.1.20/24', mac='00:00:00:00:00:02')
     h3 = net.addHost('h3', ip='10.1.2.30/24', mac='00:00:00:00:00:03')
@@ -22,6 +24,8 @@ def topology():
     net.addLink(h3, s1)
     net.addLink(h4, s1)
     net.build()
+    c0.start()
+    s1.start([c0])
     net.start()
     CLI(net)
     net.stop()
