73b8be39dca1740a97b38f3733f40671e4eb81f7
Younes Henni <younabobo3@gmail.com>
2020-06-07 17:55:37 +0100

    test

--- files/lab1.py
+++ files/lab1.py
@@ -6,26 +6,25 @@ from mininet.node import CPULimitedHost, Host, Node
 from mininet.node import OVSKernelSwitch, UserSwitch, OVSSwitch
 from mininet.node import Controller, RemoteController, OVSController
 
-class MyTopo(Topo):
-    "Simple topology example."
 
-    def __init__(self):
-        "Create custom topo."
-        net = Mininet(controller=Controller, switch=OVSSwitch)
-        # Initialize topology
-        Topo.__init__(self)
-        # Add hosts and switches
-        h1=self.addHost('h1',ip='10.1.1.10/24',mac='00:00:00:00:00:01')
-        h2=self.addHost('h2',ip='10.1.1.20/24',mac='00:00:00:00:00:02')
-        h3=self.addHost('h3',ip='10.1.2.30/24',mac='00:00:00:00:00:03')
-        h4=self.addHost('h4',ip='10.1.2.40/24',mac='00:00:00:00:00:04')
-        s1=self.addSwitch('s1')
+def topology():
+    net = Mininet(controller=RemoteController,
+                  link=TCLink, switch=OVSKernelSwitch)
+    net = Mininet(controller=Controller, switch=OVSSwitch)
+    # Initialize topology
+    Topo.__init__(net)
+    # Add hosts and switches
+    h1 = net.addHost('h1', ip='10.1.1.10/24', mac='00:00:00:00:00:01')
+    h2 = net.addHost('h2', ip='10.1.1.20/24', mac='00:00:00:00:00:02')
+    h3 = net.addHost('h3', ip='10.1.2.30/24', mac='00:00:00:00:00:03')
+    h4 = net.addHost('h4', ip='10.1.2.40/24', mac='00:00:00:00:00:04')
+    s1 = net.addSwitch('s1')
+    # Add links
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s1)
+    net.addLink(h4, s1)
+    net.stop()
 
-        # Add links
-        self.addLink(h1,s1)
-        self.addLink(h2,s1)
-        self.addLink(h3,s1)
-        self.addLink(h4,s1)
-        net.stop()
 
-topos = {'lab1':(lambda:MyTopo())}
+topos = {'lab1': (lambda: MyTopo())}
