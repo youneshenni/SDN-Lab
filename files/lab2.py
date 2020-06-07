8e5650a9e94b950a4e82cd675a79c4ee501ec1ca
Younes Henni <younabobo3@gmail.com>
2020-06-07 20:46:48 +0100

    test

--- files/lab2.py
+++ files/lab2.py
@@ -16,6 +16,7 @@ def topology():
     r1 = net.addHost('r1')
     s1 = net.addSwitch('s1')
     net.addLink(r1, s1)
+    net.addLink(r1, s1)
     net.addLink(h1, s1)
     net.addLink(h2, s1)
     net.addLink(h3, s1)
